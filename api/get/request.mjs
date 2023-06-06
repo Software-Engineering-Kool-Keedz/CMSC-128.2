33865f43deee8573aee397af09110f3bd3f934a9
Anna <Mdcarigaba@users.noreply.github.com>
2023-06-06 12:06:02 +0800

    Co-authored-by: Lindsey Mae T. Diuco <LindseyDiuco@users.noreply.github.com>

--- api/get/request.mjs
+++ api/get/request.mjs
@@ -1,1 +1,17 @@
 //search
+router.get('/patient', (req, res) => {
+    db('patient').select(['patient.first_name', 'patient.last_name', 'patient.id'])
+    .where((builder) => {
+        if(req.query['patient.first_name'] && req.query['patient.last_name']){
+             builder.whereILike('patient.first_name', `${req.query['patient.first_name']}%`)
+             .andWhereILike('patient.last_name', `${req.query['patient.last_name']}%`);
+        }
+        else if(req.query['patient.last_name']) builder.whereILike('patient.last_name', `${req.query['patient.last_name']}%`);
+        else if(req.query['patient.first_name']) builder.whereILike('patient.first_name', `${req.query['patient.first_name']}%`);
+    })
+    // .orderBy('patient.created_at', 'asc')
+    .returning()
+    .then((data) => {
+        res.json(data);
+    })
+})
